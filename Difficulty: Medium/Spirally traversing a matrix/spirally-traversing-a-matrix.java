db1c8db68d003635317ef15d8f81ea901621deb6
manuj-chadha <160269648+manuj-chadha@users.noreply.github.com>
2025-01-24 11:07:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+
+            int matrix[][] = new int[r][c];
+
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.spirallyTraverse(matrix);
+            for (Integer val : ans) System.out.print(val + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int matrix[][]) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        int top=0;
+        int bottom=matrix.length-1;
+        int left=0;
+        int right=matrix[0].length-1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    list.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return list;
+    }
+}
